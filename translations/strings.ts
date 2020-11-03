c4aff716dce72680dd26319f0362c36a9f60f21a
Damaris <d4maris@devisland.de>
2020-11-03 16:24:57 +0100

    language selection english and german

---
+++ translations/strings.ts
@@ -0,0 +1,22 @@
+import { Strings } from './types'
+
+const strings: Strings = {
+    en: {
+        about: 'About René Magritte',
+        painting: 'The Painting',
+        artist: 'The Artist',
+        notAPipe: 'This is not a pipe',
+        bio: `René François Ghislain Magritte (21 November 1898 – 15 August 1967) was a Belgian Surrealist artist. He became well known for creating a number of witty and thought-provoking images. Often depicting ordinary objects in an unusual context, his work is known for challenging observers' preconditioned perceptions of reality. His imagery has influenced pop art, minimalist art and conceptual art.`,
+        readMore: 'Read more on Wikipedia...'
+    },
+    de: {
+        about: 'Ueber René Magritte',
+        painting: 'Werke',
+        artist: "Kuenstler",
+        notAPipe: "Das ist keine Pfeife",
+        bio: `René François Ghislain Magritte (* 21. November 1898 in Lessines in der wallonischen Provinz Hennegau, Belgien; † 15. August 1967 in Brüssel) war ein belgischer Maler des Surrealismus.`,
+        readMore: 'Lese mehr auf Wikipedia...'
+    }
+}
+
+export default strings
